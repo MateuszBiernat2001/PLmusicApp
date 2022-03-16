1a4ef7b817f1d0dbaeebc88d65dbda387d21050d
= <biernat521@gmail.com>
2022-03-16 14:18:18 +0100

    Update js/json file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const itemSection = document.querySelector('.specials-music-container');
-const topItemSection = document.querySelector('.top-wrapper-music');
+//const topItemSection = document.querySelector('.top-wrapper-music');
 const searchInput = document.querySelector('.search-input');
 const errorText = document.querySelector('.error');
 const allSongsContainer = document.querySelector('.wrapper-all-songs');
@@ -27,12 +27,49 @@ const getSongs = () => {
           allSongsContainer.appendChild(item);
         }
       };
-
       getAllSongs();
 
       const getSpecialSongs = () => {
+        const specialSongs = [];
+
+        for(let i = 0; i < musicArr.length; i++){
+          if (musicArr[i].authorName.toLowerCase() === searchInput.value.toLowerCase()){
+            specialSongs.push(musicArr[i])
+          }else{
+            errorText.textContent = 'Chyba nie mamy tutaj nic dla Ciebie :('
+          }
+        }
+        for(let i = 0; i < specialSongs.length; i++){
+          const item = document.createElement('a');
+          const title = document.createElement('p');
+          const author = document.createElement('p');
+          const img = document.createElement('img');
+          item.setAttribute('href', musicArr[i].link);
+          item.classList.add('music-item');
+          item.appendChild(img);
+          item.appendChild(title);
+          item.appendChild(author);
+          title.innerHTML = specialSongs[i].title;
+          author.innerHTML = specialSongs[i].authorName;
+          img.setAttribute('src', specialSongs[i].img);
+          itemSection.appendChild(item);         
+          errorText.textContent = '';
+        }
+        searchInput.innerHTML = '';
+        specialSongs.length = 0;
+      }
+      searchInput.addEventListener('change', getSpecialSongs);
+    });
+};
+getSongs();
+
+
+
+
+/*
+      const getSpecialSongs = () => {
         for (let i = 0; i < musicArr.length; i++) {
-          if (musicArr[i].authorName === searchInput.value) {
+          if (musicArr[i].authorName.toLowerCase() === searchInput.value.toLowerCase()) {
             const item = document.createElement('a');
             const title = document.createElement('p');
             const author = document.createElement('p');
@@ -53,7 +90,4 @@ const getSongs = () => {
         searchInput.value = '';
         errorText.innerHTML = '';
       };
-      searchInput.addEventListener('change', getSpecialSongs);
-    });
-};
-getSongs();
+      searchInput.addEventListener('change', getSpecialSongs);*/ 
